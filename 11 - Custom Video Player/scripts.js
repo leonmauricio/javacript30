72d6371ca7a1eac8a616a65c7cb0892d671eaa96
Mauricio Leon <mauriciomunoz011@gmail.com>
2018-09-14 17:33:54 -0300

    HTML5 player

---
+++ 11 - Custom Video Player/scripts.js
@@ -0,0 +1,74 @@
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
+const fullscreen = player.querySelector('.fullscreen');
+
+function togglePlay(){
+	if (video.paused){
+		video.play()
+	} else {
+		video.pause();
+	};
+};
+
+function updateButton(){
+	const icon = this.paused ? '►' : '❚ ❚';
+	toggle.textContent = icon;
+}
+
+function skip(){
+	video.currentTime += parseFloat(this.dataset.skip);
+}
+
+function handleRangeUpdate(){
+	video[this.name] = this.value;
+}
+
+function handleProgress(){
+	const percent = (video.currentTime / video.duration) * 100;
+	progressBar.style.flexBasis = `${percent}%`
+}
+
+function scrub(e){
+	const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+	video.currentTime = scrubTime;
+}
+
+function toggleFullscreen(){
+	if (!document.fullscreenElement) {
+		if (video.webkitRequestFullscreen){
+			video.webkitRequestFullscreen();
+		} else if (video.mozRequestFullScreen){
+			video.mozRequestFullScreen();
+		} else if (video.msRequestFullscreen){
+			video.msRequestFullscreen();
+		}
+	} else {
+		if (document.exitFullscreen) {
+			document.exitFullscreen(); 
+		}
+	}
+}
+
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', handleProgress);
+
+toggle.addEventListener('click', togglePlay);
+
+let mouseDown = false;
+progress.addEventListener('click', scrub);
+progress.addEventListener('mousemove', (e) => mouseDown && scrub(e));
+progress.addEventListener('mousedown', () => mouseDown = true);
+progress.addEventListener('mouseup', () => mouseDown = false);
+
+fullscreen.addEventListener('click', toggleFullscreen);
+
+skipButtons.forEach(button => button.addEventListener('click', skip));
+//ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
